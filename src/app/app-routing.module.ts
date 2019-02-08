7333bcfca4e73a674f50e932b91f5d7e31da26a2
sgilbertneu17 <gilbert.sh@husky.neu.edu>
2019-02-07 20:23:34 -0800

    assignment 2 quick fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './views/user/login/login.component';
+import {ProfileComponent} from './views/user/profile/profile.component';
+import {RegisterComponent} from './views/user/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+//export const routing = RouterModule.forRoot(appRoutes)
 export class AppRoutingModule { }
